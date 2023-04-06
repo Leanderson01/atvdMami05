528570f7c0fad5cc537c134405ab0b85610acbd9
Leanderson01 <leandersonnunes.alu.lmb@gmail.com>
2023-04-06 16:22:45 -0300

    feat: Finalizando ações do carrinho

--- src/App.tsx
+++ src/App.tsx
@@ -5,34 +5,70 @@ import Carrinho from "./Carrinho";
 function App() {
   const [posicao, setPosicao] = useState(0);
   const [segundos, setSegundos] = useState(0);
+  const [start, setStart] = useState(false);
 
   useEffect(() => {
     const width = window.screen.width - 400;
-
     const intervalId = setInterval(() => {
       if (posicao > width) {
         clearInterval(intervalId);
+      } else if (start === true) {
+        setPosicao((posicao) => posicao + 80);
       } else {
-        setPosicao((posicao) => posicao + 40);
+        setPosicao((posicao) => posicao + 0);
       }
-      if (segundos > width) {
+      if (posicao > width) {
         clearInterval(intervalId);
-      } else {
+      } else if (start === true) {
         setSegundos((segundos) => segundos + 1);
+      } else {
+        setSegundos((segundos) => segundos + 0);
       }
     }, 1000);
 
     return () => clearInterval(intervalId);
-  }, [segundos]);
+  }, [segundos, start]);
+
+  function clear() {
+    setPosicao(0);
+    setSegundos(0);
+    setStart(false);
+  }
 
   return (
     <div className="App">
+      <div className="action">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setStart(true);
+          }}
+        >
+          Começar
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setStart(false);
+          }}
+        >
+          Parar
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            clear();
+          }}
+        >
+          Zerar
+        </button>
+      </div>
       <div className="infos">
         <div className="vel">
-          <h1>Velocidade: 40px</h1>
+          <h1>Velocidade: 80px</h1>
         </div>
         <div className="timer">
-          <h1>Tempo: {segundos > 25 ? 25 : segundos}</h1>
+          <h1>Tempo: {segundos}</h1>
         </div>
         <div className="displacement">
           <h1>Deslocamento: {posicao}</h1>
