d5e46f154fa0ba9d3e4cd1b59457c530c3fc0e73
Leanderson01 <leandersonnunes.alu.lmb@gmail.com>
2023-04-19 22:37:01 -0300

    feat: Adicionando aceleração

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
   const [posicao, setPosicao] = useState(0);
   const [segundos, setSegundos] = useState(0);
   const [start, setStart] = useState(false);
+  const [aceleracao, setAceleracao] = useState(0);
+  const velocidadeInicial = 80;
 
   useEffect(() => {
     const width = window.screen.width - 400;
@@ -13,7 +15,12 @@ function App() {
       if (posicao > width) {
         clearInterval(intervalId);
       } else if (start === true) {
-        setPosicao((posicao) => posicao + 80);
+        const tempo = segundos;
+        const novaPosicao =
+          posicao + velocidadeInicial * tempo + (aceleracao * tempo ** 2) / 2;
+        setPosicao(novaPosicao);
+        const novaVelocidade = velocidadeInicial + aceleracao * tempo;
+        console.log("Velocidade: ", novaVelocidade);
       } else {
         setPosicao((posicao) => posicao + 0);
       }
@@ -33,6 +40,7 @@ function App() {
     setPosicao(0);
     setSegundos(0);
     setStart(false);
+    setAceleracao(0);
   }
 
   return (
@@ -62,16 +70,32 @@ function App() {
         >
           Zerar
         </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setAceleracao((aceleracao) => aceleracao + 5);
+          }}
+        >
+          Acelerar
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setAceleracao((aceleracao) => aceleracao - 5);
+          }}
+        >
+          Desacelerar
+        </button>
       </div>
       <div className="infos">
         <div className="vel">
-          <h1>Velocidade: 80px</h1>
+          <h1>Velocidade: {velocidadeInicial}px</h1>
         </div>
         <div className="timer">
-          <h1>Tempo: {segundos}</h1>
+          <h1>Tempo: {segundos}s</h1>
         </div>
         <div className="displacement">
-          <h1>Deslocamento: {posicao}</h1>
+          <h1>Deslocamento: {posicao}px^2</h1>
         </div>
       </div>
 
